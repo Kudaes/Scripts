e5f300e374f3f1da932c951b8d67cedde06c9e21
Kudaes <kurosh_2907@hotmail.com>
2020-12-07 19:37:04 +0100

    Update Program.cs

--- Memory Patching/PatchFinder/ExportedFunctions/Program.cs
+++ Memory Patching/PatchFinder/ExportedFunctions/Program.cs
@@ -18,18 +18,8 @@ namespace ExportedFunctions
                 return;
             }
 
-            DInvoke.PE.PE_MANUAL_MAP moduleDetails = DInvoke.Map.MapModuleToMemory(@"c:\windows\system32\dbghelp.dll");
-            DInvoke.PE.PE_MANUAL_MAP moduleDetails2 = DInvoke.Map.MapModuleToMemory(@"c:\windows\system32\kernel32.dll");
-
-            Console.WriteLine("Enter username:");
-
-            // Create a string variable and get user input from the keyboard and store it in the variable
-            string userName = Console.ReadLine();
-
-
             Utils.parseArguments(args);
 
-            Thread.Sleep(1000000);
         }
     }
 }
